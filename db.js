76136b2fe2a2ffbcc711762973fb029a361921dd
Saruar <saruar.ryskaliyev@nu.edu.kz>
2024-05-16 14:51:56 +0500

    render deploy

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const {Pool} = require('pg');
 const pool = new Pool({
 
-    user: 'postgres',
-    host: 'localhost',
-    password: 'postgres',
-    database: 'postgres',
+    user: 'express_school_db_user',
+    host: 'cp2ta4i1hbls7384hjf0-a.frankfurt-postgres.render.com',
+    password: '5TfZRQ3mKJ1ZXk0gcbAAzsSeHa30HDpi',
+    database: 'express_school_db',
     port: 5432,
 });
 
