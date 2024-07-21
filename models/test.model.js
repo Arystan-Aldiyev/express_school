29b5f8c3132e6c356aeec09cea4fcfedd90d5346
dinmukhammed <artystambekdimash005@gmail.com>
2024-07-21 12:46:27 +0500

    nothing changed

--- models/test.model.js
+++ models/test.model.js
@@ -1,37 +1,37 @@
 module.exports = (sequelize, DataTypes) => {
-    return sequelize.define('Test', {
-      test_id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-      },
-      group_id: {
-        type: DataTypes.INTEGER,
-        references: {
-          model: 'groups',
-          key: 'group_id'
+    const Test = sequelize.define('Test', {
+        test_id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        group_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'groups',
+                key: 'group_id'
+            }
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        time_open: {
+            type: DataTypes.DATE
+        },
+        duration_minutes: {
+            type: DataTypes.INTEGER
+        },
+        max_attempts: {
+            type: DataTypes.INTEGER
         }
-      },
-      name: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      time_open: {
-        type: DataTypes.DATE
-      },
-      duration_minutes: {
-        type: DataTypes.INTEGER
-      },
-      max_attempts: {
-        type: DataTypes.INTEGER
-      }
     }, {
-      tableName: 'tests',
-      timestamps: false
+        tableName: 'tests',
+        timestamps: false
     });
-
-
-   
-
-
-  };
+    Test.associate = (models) => {
+        Test.hasMany(models.Question, {foreignKey: 'test_id', as: 'questions'});
+        Test.hasMany(models.Attempt, {foreignKey: 'test_id'});
+    };
+    return Test;
+};
