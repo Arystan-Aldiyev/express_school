8893c458b13c3bb0ad1d6ace6c41acce94e3fd35
dinmukhammed <artystambekdimash005@gmail.com>
2024-07-21 12:41:05 +0500

    nothing changed

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -6,7 +6,8 @@ const config = require('../config/auth.config');
 
 exports.signup = async (req, res) => {
     try {
-        const { name, email, password, role, phone_number } = req.body;
+        const {name, email, password, role, phone_number} = req.body;
+        console.log(name)
         const hashedPassword = await bcrypt.hash(password, 12);
 
         const newUser = await User.create({
@@ -17,33 +18,38 @@ exports.signup = async (req, res) => {
             phone_number
         });
 
-        const token = jwt.sign({ user_id: newUser.user_id, role: newUser.role }, config.secret, { expiresIn: '24h' });
+        const token = jwt.sign({user_id: newUser.user_id, role: newUser.role}, config.secret, {expiresIn: '24h'});
 
-        res.status(201).json({ message: "User registered successfully!", token, user_id: newUser.user_id, role: newUser.role });
+        res.status(201).json({
+            message: "User registered successfully!",
+            token,
+            user_id: newUser.user_id,
+            role: newUser.role
+        });
     } catch (error) {
-        res.status(500).send({ message: "Failed to register user.", error: error.message });
+        res.status(500).send({message: "Failed to register user.", error: error.message});
     }
 };
 
 exports.signin = async (req, res) => {
     try {
-        const { email, password } = req.body;
-        const user = await User.findOne({ where: { email } });
+        const {email, password} = req.body;
+        const user = await User.findOne({where: {email}});
 
         if (!user) {
-            return res.status(404).send({ message: "User not found." });
+            return res.status(404).send({message: "User not found."});
         }
 
         const isPasswordValid = await bcrypt.compare(password, user.password);
 
         if (!isPasswordValid) {
-            return res.status(401).send({ message: "Invalid password." });
+            return res.status(401).send({message: "Invalid password."});
         }
 
-        const token = jwt.sign({ user_id: user.user_id, role: user.role }, config.secret, { expiresIn: '24h' });
+        const token = jwt.sign({user_id: user.user_id, role: user.role}, config.secret, {expiresIn: '24h'});
 
-        res.status(200).json({ message: "User logged in successfully!", token, user_id: user.user_id, role: user.role });
+        res.status(200).json({message: "User logged in successfully!", token, user_id: user.user_id, role: user.role});
     } catch (error) {
-        res.status(500).send({ message: "Login failed.", error: error.message });
+        res.status(500).send({message: "Login failed.", error: error.message});
     }
 };
