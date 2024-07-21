128b15432ea1431231d952275004967e15547b5f
dinmukhammed <artystambekdimash005@gmail.com>
2024-07-21 12:46:11 +0500

    feat: created endpoint to analyze answers

--- controllers/test.controller.js
+++ controllers/test.controller.js
@@ -3,9 +3,8 @@ const Group = db.group;
 const Test = db.test;
 const Question = db.question;
 const AnswerOption = db.answerOption;
-
-
-
+const Attempt = db.attempt;
+const Answer = db.answer;
 
 // Create and Save a new Test
 exports.createTest = (req, res) => {
@@ -30,7 +29,6 @@ exports.createTest = (req, res) => {
             message: "Group not found!"
         });
     }
-
 
 
     Test.create(test)
@@ -95,31 +93,119 @@ exports.findTestWithDetails = (req, res) => {
             }
         ]
     })
-    .then(data => {
-        if (data) {
-            res.send(data);
-        } else {
-            res.status(404).send({
-                message: `Cannot find Test with id=${id}.`
+        .then(data => {
+            if (data) {
+                res.send(data);
+            } else {
+                res.status(404).send({
+                    message: `Cannot find Test with id=${id}.`
+                });
+            }
+        })
+        .catch(err => {
+            console.error("Error retrieving Test with id=" + id, err); // Log the actual error
+            res.status(500).send({
+                message: "Error retrieving Test with id=" + id
             });
-        }
-    })
-    .catch(err => {
-        console.error("Error retrieving Test with id=" + id, err); // Log the actual error
-        res.status(500).send({
-            message: "Error retrieving Test with id=" + id
         });
-    });
 };
 
+// Submit Test controller
+exports.submitTest = async (req, res) => {
+    const testId = req.params.id;
+    const userId = req.userId
+    console.log(req.body)
+    const {answers, startTime} = req.body;
 
+    console.log(`Received startTime: ${startTime}`);
+
+    try {
+        const test = await Test.findOne({
+            where: {test_id: testId},
+            include: [{
+                model: Question,
+                as: 'questions',
+                include: [{
+                    model: AnswerOption,
+                    as: 'answerOptions'
+                }]
+            }]
+        });
+
+        if (!test) {
+            return res.status(404).json({message: 'Test not found'});
+        }
+
+        // const groupMembership = await GroupMembership.findOne({
+        //     where: {
+        //         group_id: test.group_id,
+        //         user_id: userId
+        //     }
+        // });
+        //
+        // if (!groupMembership) {
+        //     return res.status(403).json({message: 'User not a member of the test group'});
+        // }
+
+        const attemptsCount = await Attempt.count({
+            where: {
+                test_id: testId,
+                user_id: userId
+            }
+        });
+
+        if (attemptsCount >= test.max_attempts) {
+            return res.status(403).json({message: 'Maximum attempts reached'});
+        }
+        let score = 0;
+        const endTime = new Date();
+
+        const parsedStartTime = new Date(startTime);
+        if (isNaN(parsedStartTime.getTime())) {
+            return res.status(400).json({message: 'Invalid start time'});
+        }
+
+        const attempt = await Attempt.create({
+            test_id: testId,
+            user_id: userId,
+            start_time: parsedStartTime,
+            end_time: endTime,
+            score: 0
+        });
+
+        for (const question of test.questions) {
+            const userAnswer = answers[question.question_id];
+            await Answer.create({
+                question_id: question.question_id,
+                user_id: userId,
+                student_answer: userAnswer,
+                attempt_id: attempt.attempt_id
+            });
+
+            const correctOption = question.answerOptions.find(option => option.is_correct);
+            if (userAnswer === correctOption.option_text) {
+                score++;
+            }
+        }
+
+        attempt.score = score;
+        await attempt.save();
+
+        const timeTaken = (endTime - parsedStartTime) / 1000;
+
+        res.json({score, timeTaken});
+    } catch (error) {
+        console.error(`Error submitting test with id=${testId}`, error);
+        res.status(500).json({message: 'Server error', error});
+    }
+};
 
 // Update a Test by the id in the request
 exports.updateTest = (req, res) => {
     const id = req.params.id;
 
     Test.update(req.body, {
-        where: { test_id: id }
+        where: {test_id: id}
     })
         .then(num => {
             if (num == 1) {
@@ -144,7 +230,7 @@ exports.deleteTest = (req, res) => {
     const id = req.params.id;
 
     Test.destroy({
-        where: { test_id: id }
+        where: {test_id: id}
     })
         .then(num => {
             if (num == 1) {
