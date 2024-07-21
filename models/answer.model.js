75d1ac950ec55f9863e7b69be22ae7dd6c341edc
dinmukhammed <artystambekdimash005@gmail.com>
2024-07-21 12:38:59 +0500

    updated: updated associates between questions and user

--- models/answer.model.js
+++ models/answer.model.js
@@ -1,41 +1,47 @@
 module.exports = (sequelize, DataTypes) => {
-    return sequelize.define('Answer', {
-      answer_id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-      },
-      question_id: {
-        type: DataTypes.INTEGER,
-        references: {
-          model: 'questions',
-          key: 'question_id'
+    const Answer = sequelize.define('Answer', {
+        answer_id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        question_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'questions',
+                key: 'question_id'
+            }
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'users',
+                key: 'user_id'
+            }
+        },
+        student_answer: {
+            type: DataTypes.STRING
+        },
+        attempt_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'attempts',
+                key: 'attempt_id'
+            }
+        },
+        submitted_at: {
+            type: DataTypes.DATE,
+            defaultValue: DataTypes.NOW
         }
-      },
-      user_id: {
-        type: DataTypes.INTEGER,
-        references: {
-          model: 'users',
-          key: 'user_id'
-        }
-      },
-      student_answer: {
-        type: DataTypes.STRING
-      },
-      attempt_id: {
-        type: DataTypes.INTEGER,
-        references: {
-            model: 'attempts',
-            key: 'attempt_id'
-        }
-      },
-      submitted_at: {
-        type: DataTypes.DATE,
-        defaultValue: DataTypes.NOW
-      }
     }, {
-      tableName: 'answers',
-      timestamps: false
+        tableName: 'answers',
+        timestamps: false
     });
-  };
+    Answer.associate = (models) => {
+        Answer.belongsTo(models.Question, {foreignKey: 'question_id', as: 'Question'});
+        Answer.belongsTo(models.User, {foreignKey: 'user_id'});
+    };
+
+    return Answer;
+};
   
