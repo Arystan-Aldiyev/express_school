64008afe2b1696497241d8e591bb8ccaa685b626
dinmukhammed <artystambekdimash005@gmail.com>
2024-07-21 12:40:19 +0500

    updated: associate name between answers and user

--- models/attempt.model.js
+++ models/attempt.model.js
@@ -1,36 +1,44 @@
 module.exports = (sequelize, DataTypes) => {
-    return sequelize.define('Attempt', {
-      attempt_id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-      },
-      test_id: {
-        type: DataTypes.INTEGER,
-        references: {
-          model: 'tests',
-          key: 'test_id'
+    const Attempt = sequelize.define('Attempt', {
+        attempt_id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        test_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'tests',
+                key: 'test_id'
+            }
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'users',
+                key: 'user_id'
+            }
+        },
+        start_time: {
+            type: DataTypes.DATE
+        },
+        end_time: {
+            type: DataTypes.DATE
+        },
+        score: {
+            type: DataTypes.INTEGER
         }
-      },
-      user_id: {
-        type: DataTypes.INTEGER,
-        references: {
-          model: 'users',
-          key: 'user_id'
-        }
-      },
-      start_time: {
-        type: DataTypes.DATE
-      },
-      end_time: {
-        type: DataTypes.DATE
-      },
-      score: {
-        type: DataTypes.INTEGER
-      }
     }, {
-      tableName: 'attempts',
-      timestamps: false
+        tableName: 'attempts',
+        timestamps: false
     });
-  };
+
+    Attempt.associate = (models) => {
+        Attempt.belongsTo(models.Test, {foreignKey: 'test_id', as: 'Test'});
+        Attempt.hasMany(models.Answer, {foreignKey: 'attempt_id'});
+        Attempt.belongsTo(models.User, {foreignKey: 'user_id'});
+    };
+
+    return Attempt;
+};
   
