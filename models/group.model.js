23322f3d7cfa779b37f6674486d4141c4e894c8e
dinmukhammed <artystambekdimash005@gmail.com>
2024-07-21 12:44:06 +0500

    updated: associate between group

--- models/group.model.js
+++ models/group.model.js
@@ -1,28 +1,36 @@
 module.exports = (sequelize, DataTypes) => {
-    return sequelize.define('Group', {
-      group_id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-      },
-      name: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      teacher_id: {
-        type: DataTypes.INTEGER,
-        references: {
-          model: 'users',
-          key: 'user_id'
+    const Group = sequelize.define('Group', {
+        group_id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        teacher_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'users',
+                key: 'user_id'
+            }
+        },
+        invite_code: {
+            type: DataTypes.STRING,
+            unique: true
         }
-      },
-      invite_code: {
-        type: DataTypes.STRING,
-        unique: true
-      }
     }, {
-      tableName: 'groups',
-      timestamps: false
+        tableName: 'groups',
+        timestamps: false
     });
-  };
-  
+
+    Group.associate = models => {
+        Group.hasMany(models.Schedule, {
+            foreignKey: 'group_id',
+            as: 'schedules'
+        });
+    };
+
+    return Group;
+};
