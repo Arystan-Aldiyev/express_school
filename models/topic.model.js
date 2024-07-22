79a47b21a9aaa951daf7e51cfcc8828eb05f74a9
arystambek-dimash <arystambekdimash005@gmail.com>
2024-07-22 18:02:01 +0500

    feat: topic model initialized

---
+++ models/topic.model.js
@@ -0,0 +1,23 @@
+module.exports = (sequelize, DataTypes) => {
+    return sequelize.define('Topic', {
+        topic_id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        lesson_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'lessons',
+                key: 'lesson_id'
+            }
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    }, {
+        tableName: 'topics',
+        timestamps: true
+    });
+};
