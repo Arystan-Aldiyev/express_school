47479b13a7bbdc432a43d24635f231d04955259c
arystambek-dimash <arystambekdimash005@gmail.com>
2024-07-22 18:01:26 +0500

    updated: lesson model added attributes

--- models/lesson.model.js
+++ models/lesson.model.js
@@ -1,3 +1,23 @@
 module.exports = (sequelize, DataTypes) => {
-
-}
+    return sequelize.define('Lesson', {
+        lesson_id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        group_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'groups',
+                key: 'group_id'
+            }
+        },
+        section_title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    }, {
+        tableName: 'lessons',
+        timestamps: true
+    });
+};
